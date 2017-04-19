9a1b69926311588c495d332439c899cde32d7b82
noysh22 <noy.sh22@gmail.com>
2017-04-19 18:29:41 +0200

    Commit forgotten file

---
+++ IronPortIntegration.DLL/Common/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siemplify.Integrations.IronPort.Common
+{
+    public class Settings
+    {
+        public static readonly string IronPortHost = "IronPort Host";
+        public static readonly string IronPortUserName = "IronPort User";
+        public static readonly string IronPortKey = "IronPort Key";
+
+    }
+}
